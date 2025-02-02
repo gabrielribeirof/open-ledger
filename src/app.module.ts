b9083d93adbd6a50ff26535ff5417741a4ffe4f7
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-02-02 19:19:22 -0300

    chore: remove unused user repositories implementations

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { enviromentVariablesSchema } from './enviroment-variables-schema';
 import { APP_FILTER } from '@nestjs/core';
 import { ErrorFilter } from './shared/filters/error.filter';
 import { TRANSFER_REPOSITORY } from './domain/transfer/itransfer.repository';
-import { USER_REPOSITORY } from './domain/user/iuser.repository';
 import { WALLET_REPOSITORY } from './domain/wallet/iwallet.repository';
 import { CreateP2PTransferService } from './services/create-p2p-transfer.service';
 import { CreateP2PTransferDomainService } from './domain/services/create-p2p-transfer.domain-service';
@@ -20,7 +19,6 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { UNIT_OF_WORK } from './shared/seedwork/iunit-of-work';
 import { MikroOrmUnitOfWork } from './infrastructure/repositories/mikro-orm/mikro-orm.unit-of-work';
 import { MikroOrmTransferRepository } from './infrastructure/repositories/mikro-orm/mikro-orm-transfer.repository';
-import { MikroOrmUserRepository } from './infrastructure/repositories/mikro-orm/mikro-orm-user.repository';
 import { MikroOrmWalletRepository } from './infrastructure/repositories/mikro-orm/mikro-orm-wallet.repository';
 import { NativeErrorFilter } from './shared/filters/native-error.filter';
 
@@ -40,10 +38,6 @@ import { NativeErrorFilter } from './shared/filters/native-error.filter';
 		{
 			provide: TRANSFER_REPOSITORY,
 			useClass: MikroOrmTransferRepository,
-		},
-		{
-			provide: USER_REPOSITORY,
-			useClass: MikroOrmUserRepository,
 		},
 		{
 			provide: WALLET_REPOSITORY,
