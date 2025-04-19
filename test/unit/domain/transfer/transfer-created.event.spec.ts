17c439ec3357978b0c67f0b0da162ed193a51102
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-04-19 01:25:43 +0000

    test: create transfer created event test

---
+++ test/unit/domain/transfer/transfer-created.event.spec.ts
@@ -0,0 +1,22 @@
+import { UniqueIdentifier } from '@/shared/seedwork/unique-identifier';
+import { Transfer } from '@/domain/transfer/transfer';
+import { TransferCreatedEvent } from '@/domain/transfer/transfer-created.event';
+import { Monetary } from '@/shared/domain/monetary';
+
+describe('TransferCreatedEvent', () => {
+	let transfer: Transfer;
+
+	beforeEach(() => {
+		transfer = Transfer.create({
+			originId: new UniqueIdentifier(),
+			targetId: new UniqueIdentifier(),
+			amount: Monetary.create(100).getRight(),
+		}).getRight();
+	});
+
+	it('should return the transfer id when getAggregateId is called', () => {
+		const transferCreatedEvent = new TransferCreatedEvent(transfer);
+
+		expect(transferCreatedEvent.getAggregateId()).toBe(transfer.id);
+	});
+});
