455c9d4a76ca41bbe81beda36a8b144cf5ef91e4
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-04-19 01:26:21 +0000

    test: improve create p2p transfer tests coverage and test statements

--- test/unit/domain/services/create-p2p-transfer.domain-service.spec.ts
+++ test/unit/domain/services/create-p2p-transfer.domain-service.spec.ts
@@ -7,6 +7,8 @@ import { InsufficientWalletTypePermissionsError } from '@/shared/domain/_errors/
 import { UnauthorizedTransferError } from '@/shared/domain/_errors/unauthorized-transfer.error';
 import { createFakeWallet } from '@test/helpers/wallet.helpers';
 import { InMemoryUnitOfWork } from '@/infrastructure/repositories/in-memory/in-memory.unit-of-work';
+import { Error } from '@/shared/seedwork/error';
+import { InternalServerError } from '@/shared/domain/_errors/internal-server.error';
 
 function createSut(isTransferAuthorized = true) {
 	const unitOfWork = new InMemoryUnitOfWork();
@@ -22,7 +24,7 @@ function createSut(isTransferAuthorized = true) {
 }
 
 describe('CreateP2PTransferDomainService', () => {
-	it('should not create a transfer if the originating wallet type is merchant', async () => {
+	it('should not create a transfer if the originating wallet type is different from common', async () => {
 		const origin = createFakeWallet({ type: WalletType.MERCHANT });
 		const target = createFakeWallet({ type: WalletType.COMMON });
 
@@ -53,6 +55,40 @@ describe('CreateP2PTransferDomainService', () => {
 		expect(
 			createSut(false).sut.execute(origin, target, amount),
 		).rejects.toBeInstanceOf(UnauthorizedTransferError);
+	});
+
+	it('should not create a transfer if transfer creation fails', async () => {
+		const origin = createFakeWallet({ type: WalletType.COMMON });
+		const target = createFakeWallet({ type: WalletType.COMMON });
+
+		const amount = Monetary.create(-100).getRight();
+
+		expect(
+			createSut().sut.execute(origin, target, amount),
+		).rejects.toBeInstanceOf(Error);
+	});
+
+	it('should not create a transfer when unit of work fails', async () => {
+		const origin = createFakeWallet({ type: WalletType.COMMON, balance: 100 });
+		const target = createFakeWallet({ type: WalletType.COMMON });
+
+		const amount = Monetary.create(100).getRight();
+
+		const unitOfWork = new InMemoryUnitOfWork();
+		unitOfWork.commit = jest.fn().mockRejectedValueOnce('Unit of work failed');
+		unitOfWork.rollback = jest.fn();
+
+		const sut = new CreateP2PTransferDomainService(
+			new InMemoryTransferAuthorizerProvider(true),
+			unitOfWork,
+		);
+
+		await expect(sut.execute(origin, target, amount)).rejects.toBeInstanceOf(
+			InternalServerError,
+		);
+
+		expect(unitOfWork.rollback).toHaveBeenCalled();
+		expect(unitOfWork.commit).toHaveBeenCalled();
 	});
 
 	it('should create a transfer if all conditions are met', async () => {
