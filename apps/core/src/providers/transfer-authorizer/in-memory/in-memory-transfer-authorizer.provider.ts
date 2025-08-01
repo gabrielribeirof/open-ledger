60272643af7438c271a9d5b81850f0c5de93e5a7
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-08-01 07:00:00 -0300

    refactor: remove no longer used providers

--- apps/core/src/providers/transfer-authorizer/in-memory/in-memory-transfer-authorizer.provider.ts
+++
@@ -1,9 +0,0 @@
-import { ITransferAuthorizerProvider } from '../itransfer-authorizer.provider'
-
-export class InMemoryTransferAuthorizerProvider implements ITransferAuthorizerProvider {
-	constructor(private mockedResult = true) {}
-
-	async execute(): Promise<boolean> {
-		return this.mockedResult
-	}
-}
--- apps/core/src/providers/transfer-authorizer/itransfer-authorizer.provider.ts
+++
@@ -1,8 +0,0 @@
-import { Account } from '@/domain/account/account'
-import { Monetary } from '@/shared/domain/monetary'
-
-export interface ITransferAuthorizerProvider {
-	execute(originAccount: Account, targetAccount: Account, amount: Monetary): Promise<boolean>
-}
-
-export const TRANSFER_AUTHORIZER_PROVIDER = 'ITransferAuthorizerProvider'
