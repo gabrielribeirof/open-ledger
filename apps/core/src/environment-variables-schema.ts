b74b1d3ce5f78063c8cdced8da379e18227922fa
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-08-01 07:00:00 -0300

    refactor: remove providers no longer used env vars

--- apps/core/src/environment-variables-schema.ts
+++ apps/core/src/environment-variables-schema.ts
@@ -1,19 +1,13 @@
-import Joi from 'joi'
+import * as Joi from 'joi'
 
 export const environmentVariablesSchema = Joi.object({
 	NODE_ENV: Joi.string().valid('development', 'production', 'test').required(),
-	TRANSFER_AUTHORIZER_SERVICE_PROVIDER: Joi.string().valid('in-memory', 'devitools').required(),
-	TRANSFER_AUTHORIZER_SERVICE_URL: Joi.string().required(),
-	NOTIFY_SERVICE_URL: Joi.string().required(),
 	HTTP_PORT: Joi.number().required(),
 	DATABASE_URI: Joi.string().required(),
 })
 
 export const ENVIRONMENT_VARIABLES = {
 	NODE_ENV: 'NODE_ENV',
-	TRANSFER_AUTHORIZER_SERVICE_PROVIDER: 'TRANSFER_AUTHORIZER_SERVICE_PROVIDER',
-	TRANSFER_AUTHORIZER_SERVICE_URL: 'TRANSFER_AUTHORIZER_SERVICE_URL',
-	NOTIFY_SERVICE_URL: 'NOTIFY_SERVICE_URL',
 	HTTP_PORT: 'HTTP_PORT',
 	DATABASE_URI: 'DATABASE_URI',
 }
