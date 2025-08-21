5387893825161361faded49cd6b3d6acc6d66dc0
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-08-21 07:00:00 -0300

    chore: add aggregate-root domainEvents property readonly flag

--- apps/core/src/shared/seedwork/aggregate-root.ts
+++ apps/core/src/shared/seedwork/aggregate-root.ts
@@ -2,7 +2,7 @@ import { Entity } from './entity'
 import { IDomainEvent } from './idomain-event'
 
 export abstract class AggregateRoot<T> extends Entity<T> {
-	private _domainEvents: IDomainEvent[] = []
+	private readonly _domainEvents: IDomainEvent[] = []
 
 	get domainEvents() {
 		return this._domainEvents
