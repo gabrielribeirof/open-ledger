6279a1444712df2cf4ffa83b0f18f4b38bbfe4ef
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-08-29 07:00:00 -0300

    feat: add account alias properties length validation

--- apps/core/src/domain/account/account-alias.ts
+++ apps/core/src/domain/account/account-alias.ts
@@ -1,15 +1,25 @@
+import { BadLengthViolation } from '@/shared/domain/_errors/violations/bad-length.violation'
+import { Either, left, right } from '@/shared/lib/either'
 import { ValueObject } from '@/shared/seedwork/value-object'
+import { Violation } from '@/shared/seedwork/violation'
 
 interface AccountAliasProperties {
 	value: string
 }
 
 export class AccountAlias extends ValueObject<AccountAliasProperties> {
+	static readonly MIN_LENGTH = 3
+	static readonly MAX_LENGTH = 64
+
 	get value(): string {
 		return this.properties.value
 	}
 
-	public static create(value: string): AccountAlias {
-		return new AccountAlias({ value })
+	public static create(value: string): Either<Violation, AccountAlias> {
+		if (value.length > this.MAX_LENGTH || value.length < this.MIN_LENGTH) {
+			return left(new BadLengthViolation(this.MIN_LENGTH, this.MAX_LENGTH))
+		}
+
+		return right(new AccountAlias({ value }))
 	}
 }
