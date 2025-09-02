af95a27c7ee47536a0c36b7abfb747c13a2b7f2a
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-09-02 07:00:00 -0300

    feat: import new artifacts in app.module

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -8,13 +8,20 @@ import { ASSET_REPOSITORY_TOKEN } from './domain/asset/iasset.repository'
 import { CreateTransactionDomainService } from './domain/services/create-transaction.domain-service'
 import { TRANSACTION_REPOSITORY_TOKEN } from './domain/transaction/itransaction.repository'
 import { environmentVariablesSchema } from './environment-variables-schema'
+import { AccountController } from './infrastructure/http/controllers/account.controller'
+import { AssetController } from './infrastructure/http/controllers/asset.controller'
 import { TransactionController } from './infrastructure/http/controllers/transaction.controller'
 import { InMemoryUnitOfWork } from './infrastructure/repositories/in-memory/in-memory.unit-of-work'
 import { InMemoryAccountRepository } from './infrastructure/repositories/in-memory/in-memory-account.repository'
 import { InMemoryAssetRepository } from './infrastructure/repositories/in-memory/in-memory-asset.repository'
 import { InMemoryTransactionRepository } from './infrastructure/repositories/in-memory/in-memory-transaction.repository'
+import { CreateAccountService } from './services/create-account.service'
+import { CreateAssetService } from './services/create-asset.service'
 import { CreateTransactionService } from './services/create-transaction.service'
+import { GetAccountByAliasService } from './services/get-account-by-alias.service'
+import { GetAssetByCodeService } from './services/get-asset-by-code.service'
 import { ErrorFilter } from './shared/filters/error.filter'
+import { HttpExceptionFilter } from './shared/filters/http-exception.filter'
 import { NativeErrorFilter } from './shared/filters/native-error.filter'
 import { UNIT_OF_WORK } from './shared/seedwork/iunit-of-work'
 
@@ -44,17 +51,25 @@ import { UNIT_OF_WORK } from './shared/seedwork/iunit-of-work'
 			provide: UNIT_OF_WORK,
 			useClass: InMemoryUnitOfWork,
 		},
+		CreateAssetService,
+		GetAssetByCodeService,
+		CreateAccountService,
+		GetAccountByAliasService,
 		CreateTransactionDomainService,
 		CreateTransactionService,
+		{
+			provide: APP_FILTER,
+			useClass: NativeErrorFilter,
+		},
 		{
 			provide: APP_FILTER,
 			useClass: ErrorFilter,
 		},
 		{
 			provide: APP_FILTER,
-			useClass: NativeErrorFilter,
+			useClass: HttpExceptionFilter,
 		},
 	],
-	controllers: [TransactionController],
+	controllers: [AssetController, AccountController, TransactionController],
 })
 export class AppModule {}
