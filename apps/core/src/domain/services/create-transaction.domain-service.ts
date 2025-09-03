c656e1f66a7d1e9a2b8f260aa01c9c5aedf07ef8
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-09-03 07:00:00 -0300

    feat: remove create-transaction domain service transaction control and add new input

--- apps/core/src/domain/services/create-transaction.domain-service.ts
+++ apps/core/src/domain/services/create-transaction.domain-service.ts
@@ -1,43 +1,20 @@
-import { Inject, Injectable } from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 
+import { TransactionAccountAssetMismatchError } from '@/shared/domain/_errors/transaction-account-asset-mismatch.error'
 import { Amount } from '@/shared/domain/amount'
-import { IUnitOfWork, UNIT_OF_WORK } from '@/shared/seedwork/iunit-of-work'
 
 import { Account } from '../account/account'
-import { Asset } from '../asset/asset'
 import { Operation } from '../transaction/operation'
 import { OperationType } from '../transaction/operation-type'
 import { Transaction } from '../transaction/transaction'
-
-interface AmountDistributionStrategy {
-	amount: Amount
-}
-
-interface ShareDistributionStrategy {
-	share: number
-}
-
-interface RemainingDistributionStrategy {
-	remaining: true
-}
-
-type DistributionStrategy = AmountDistributionStrategy | ShareDistributionStrategy | RemainingDistributionStrategy
-
-export type Distribution = DistributionStrategy & {
-	account: Account
-}
-
-export interface CreateTransactionDomainServiceInput {
-	amount: Amount
-	asset: Asset
-	sources: Distribution[]
-	targets: Distribution[]
-}
+import { AmountDistribution } from './inputs/children/amount-distribution'
+import { Distribution } from './inputs/children/distribution'
+import { RemainingDistribution } from './inputs/children/remaining-distribution'
+import { ShareDistribution } from './inputs/children/share-distribution'
+import { CreateTransactionDomainServiceInput } from './inputs/create-transaction.domain-service.input'
 
 @Injectable()
 export class CreateTransactionDomainService {
-	constructor(@Inject(UNIT_OF_WORK) private readonly unitOfWork: IUnitOfWork) {}
-
 	public async execute(input: CreateTransactionDomainServiceInput): Promise<Transaction> {
 		const sourceAmountPerAccount = this.calculateAmountPerAccountInADistribution(input.sources, input.amount)
 		const targetAmountPerAccount = this.calculateAmountPerAccountInADistribution(input.targets, input.amount)
@@ -68,6 +45,32 @@ export class CreateTransactionDomainService {
 				}),
 			)
 
+		for (const operation of operations) {
+			if (operation.isDebit()) {
+				const accountAmount = sourceAmountPerAccount.find((source) => source.account.id.equals(operation.account_id))
+
+				if (!accountAmount) throw new Error(`Amount not calculated for source account ${operation.account_id.value}`)
+
+				if (!accountAmount.account.asset_code.equals(input.asset.code)) {
+					throw new TransactionAccountAssetMismatchError()
+				}
+
+				accountAmount.account.withdraw(accountAmount.amount)
+			}
+
+			if (operation.isCredit()) {
+				const accountAmount = targetAmountPerAccount.find((target) => target.account.id.equals(operation.account_id))
+
+				if (!accountAmount) throw new Error(`Amount not calculated for target account ${operation.account_id.value}`)
+
+				if (!accountAmount.account.asset_code.equals(input.asset.code)) {
+					throw new TransactionAccountAssetMismatchError()
+				}
+
+				accountAmount.account.deposit(accountAmount.amount)
+			}
+		}
+
 		const transaction = Transaction.create({
 			asset_id: input.asset.id,
 			amount: input.amount,
@@ -76,56 +79,26 @@ export class CreateTransactionDomainService {
 
 		if (transaction.isLeft()) throw transaction.value
 
-		this.unitOfWork.begin()
-
-		try {
-			for (const operation of transaction.value.operations) {
-				if (operation.isDebit()) {
-					const accountAmount = sourceAmountPerAccount.find((source) => source.account.id.equals(operation.account_id))
-
-					if (!accountAmount) throw new Error(`Amount not calculated for source account ${operation.account_id.value}`)
-
-					accountAmount.account.withdraw(accountAmount.amount)
-					await this.unitOfWork.accountRepository.save(accountAmount.account)
-				}
-
-				if (operation.isCredit()) {
-					const accountAmount = targetAmountPerAccount.find((target) => target.account.id.equals(operation.account_id))
-
-					if (!accountAmount) throw new Error(`Amount not calculated for target account ${operation.account_id.value}`)
-
-					accountAmount.account.deposit(accountAmount.amount)
-					await this.unitOfWork.accountRepository.save(accountAmount.account)
-				}
-			}
-
-			await this.unitOfWork.transactionRepository.save(transaction.value)
-			await this.unitOfWork.commit()
-		} catch (error) {
-			await this.unitOfWork.rollback(error)
-			throw error
-		}
-
 		return transaction.value
 	}
 
 	private calculateAmountPerAccountInADistribution(distributions: Distribution[], totalAmount: Amount) {
-		const result: Array<{ account: Account; amount: Amount }> = []
+		const result: { account: Account; amount: Amount }[] = []
 
 		let remaining = Amount.create({ value: totalAmount.value, scale: totalAmount.scale }).getRight()
 
 		for (const distribution of distributions) {
-			if ('share' in distribution) {
+			if (distribution instanceof ShareDistribution) {
 				const shareAmount = totalAmount.percentage(distribution.share)
 				remaining = remaining.subtract(shareAmount)
 				result.push({ account: distribution.account, amount: shareAmount })
-			} else if ('amount' in distribution) {
+			} else if (distribution instanceof AmountDistribution) {
 				remaining = remaining.subtract(distribution.amount)
 				result.push({ account: distribution.account, amount: distribution.amount })
 			}
 		}
 
-		const remainingDistributions = distributions.filter((distribution) => 'remaining' in distribution)
+		const remainingDistributions = distributions.filter((distribution) => distribution instanceof RemainingDistribution)
 
 		if (remainingDistributions.length > 0) {
 			for (const distribution of remainingDistributions) {
