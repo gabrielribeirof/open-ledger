328fac74c05599765027e94aa1fb29f78579656c
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-09-04 07:00:00 -0300

    chore: remove generateFakeUnitOfWork props

--- apps/core/test/helpers/unit-of-work.helpers.ts
+++ apps/core/test/helpers/unit-of-work.helpers.ts
@@ -1,17 +1,13 @@
 import { InMemoryAccountRepository } from '@/infrastructure/repositories/in-memory/in-memory-account.repository'
 import { InMemoryTransactionRepository } from '@/infrastructure/repositories/in-memory/in-memory-transaction.repository'
-import { IUnitOfWork } from '@/shared/seedwork/iunit-of-work'
+import type { IUnitOfWork } from '@/shared/seedwork/iunit-of-work'
 
-interface GenerateFakeUnitOfWorkProperties {
-	commit?: jest.Mock
-}
-
-export function generateFakeUnitOfWork(props: GenerateFakeUnitOfWorkProperties = {}): IUnitOfWork {
+export function generateFakeUnitOfWork(): IUnitOfWork {
 	return {
 		transactionRepository: new InMemoryTransactionRepository(),
 		accountRepository: new InMemoryAccountRepository(),
 		begin: jest.fn().mockResolvedValue(undefined),
-		commit: props.commit ?? jest.fn().mockResolvedValue(undefined),
+		commit: jest.fn().mockResolvedValue(undefined),
 		rollback: jest.fn().mockResolvedValue(undefined),
 	}
 }
