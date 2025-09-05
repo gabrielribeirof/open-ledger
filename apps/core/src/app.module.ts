98fbc12561a1db7504ff6142580f35aa716cc68d
gabrielribeirof <39045611+gabrielribeirof@users.noreply.github.com>
2025-09-05 07:00:00 -0300

    feat(app.module): add new artifacts references

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -1,20 +1,25 @@
 import { HttpModule } from '@nestjs/axios'
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { APP_FILTER } from '@nestjs/core'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { ACCOUNT_REPOSITORY_TOKEN } from './domain/account/iaccount.repository'
 import { ASSET_REPOSITORY_TOKEN } from './domain/asset/iasset.repository'
 import { CreateTransactionDomainService } from './domain/services/create-transaction.domain-service'
 import { TRANSACTION_REPOSITORY_TOKEN } from './domain/transaction/itransaction.repository'
-import { environmentVariablesSchema } from './environment-variables-schema'
+import { ENVIRONMENT_VARIABLES, environmentVariablesSchema } from './environment-variables-schema'
 import { AccountController } from './infrastructure/http/controllers/account.controller'
 import { AssetController } from './infrastructure/http/controllers/asset.controller'
 import { TransactionController } from './infrastructure/http/controllers/transaction.controller'
-import { InMemoryUnitOfWork } from './infrastructure/repositories/in-memory/in-memory.unit-of-work'
-import { InMemoryAccountRepository } from './infrastructure/repositories/in-memory/in-memory-account.repository'
-import { InMemoryAssetRepository } from './infrastructure/repositories/in-memory/in-memory-asset.repository'
-import { InMemoryTransactionRepository } from './infrastructure/repositories/in-memory/in-memory-transaction.repository'
+import { TypeORMUnitOfWork } from './infrastructure/repositories/typeorm/typeorm.unit-of-work'
+import { TypeORMAccountRepository } from './infrastructure/repositories/typeorm/typeorm-account.repository'
+import { TypeORMAssetRepository } from './infrastructure/repositories/typeorm/typeorm-asset.repository'
+import { TypeORMTransactionRepository } from './infrastructure/repositories/typeorm/typeorm-transaction.repository'
+import { AccountEntity } from './infrastructure/typeorm/entities/account.entity'
+import { AssetEntity } from './infrastructure/typeorm/entities/asset.entity'
+import { OperationEntity } from './infrastructure/typeorm/entities/operation.entity'
+import { TransactionEntity } from './infrastructure/typeorm/entities/transaction.entity'
 import { CreateAccountService } from './services/create-account.service'
 import { CreateAssetService } from './services/create-asset.service'
 import { CreateTransactionService } from './services/create-transaction.service'
@@ -23,7 +28,7 @@ import { GetAssetByCodeService } from './services/get-asset-by-code.service'
 import { ErrorFilter } from './shared/filters/error.filter'
 import { HttpExceptionFilter } from './shared/filters/http-exception.filter'
 import { NativeErrorFilter } from './shared/filters/native-error.filter'
-import { UNIT_OF_WORK } from './shared/seedwork/iunit-of-work'
+import { UNIT_OF_WORK_TOKEN } from './shared/seedwork/iunit-of-work'
 
 @Module({
 	imports: [
@@ -32,24 +37,34 @@ import { UNIT_OF_WORK } from './shared/seedwork/iunit-of-work'
 			ignoreEnvFile: false,
 			validationSchema: environmentVariablesSchema,
 		}),
+		TypeOrmModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: async (configService: ConfigService) => ({
+				type: 'postgres',
+				url: configService.get<string>(ENVIRONMENT_VARIABLES.DATABASE_URI),
+				autoLoadEntities: true,
+			}),
+		}),
+		TypeOrmModule.forFeature([AssetEntity, AccountEntity, TransactionEntity, OperationEntity]),
 		HttpModule,
 	],
 	providers: [
 		{
 			provide: ACCOUNT_REPOSITORY_TOKEN,
-			useClass: InMemoryAccountRepository,
+			useClass: TypeORMAccountRepository,
 		},
 		{
 			provide: TRANSACTION_REPOSITORY_TOKEN,
-			useClass: InMemoryTransactionRepository,
+			useClass: TypeORMTransactionRepository,
 		},
 		{
 			provide: ASSET_REPOSITORY_TOKEN,
-			useClass: InMemoryAssetRepository,
+			useClass: TypeORMAssetRepository,
 		},
 		{
-			provide: UNIT_OF_WORK,
-			useClass: InMemoryUnitOfWork,
+			provide: UNIT_OF_WORK_TOKEN,
+			useClass: TypeORMUnitOfWork,
 		},
 		CreateAssetService,
 		GetAssetByCodeService,
